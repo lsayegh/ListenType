73a156de0344d32680cc1d74caab3cec3c8cfba2
lsayegh <luis.sayegh@gmail.com>
2015-08-14 08:48:18 +0100

    First check-in

---
+++ js/javascript.js
@@ -0,0 +1,292 @@
+var lyric = new Array();
+
+	lyric[0] = new Array()
+	lyric[0][0] = 'Coming';
+	lyric[0][1] = 'out';
+	lyric[0][2] = 'of';
+	lyric[0][3] = 'my';
+	lyric[0][4] = 'cage';
+	
+	lyric[1] = new Array()
+	lyric[1][0] = 'And';
+	lyric[1][1] = 'I\'ve';
+	lyric[1][2] = 'been';
+	lyric[1][3] = 'doing';
+	lyric[1][4] = 'just';
+	lyric[1][5] = 'fine';
+
+	lyric[2] = new Array()
+	lyric[2][0] = 'Gotta';
+	lyric[2][1] = 'Gotta';
+	lyric[2][2] = 'be';
+	lyric[2][3] = 'down';
+	
+	lyric[3] = new Array()
+	lyric[3][0] = 'Because';
+	lyric[3][1] = 'I';
+	lyric[3][2] = 'want';
+	lyric[3][3] = 'it';
+	lyric[3][4] = 'all';
+
+	lyric[4] = new Array()
+	lyric[4][0] = 'How'
+	lyric[4][1] = 'did'
+	lyric[4][2] = 'it'
+	lyric[4][3] = 'end'
+	lyric[4][4] = 'up'
+	lyric[4][5] = 'like'
+	lyric[4][6] = 'this'
+
+	lyric[5] = new Array()
+	lyric[5][0] = 'It'
+	lyric[5][1] = 'was'
+	lyric[5][2] = 'only'
+	lyric[5][3] = 'a'
+	lyric[5][4] = 'kiss'
+	lyric[5][4] = ','
+	lyric[5][5] = 'it'
+	lyric[5][5] = 'was'
+	lyric[5][5] = 'only'
+	lyric[5][5] = 'a'
+	lyric[5][5] = 'kiss'
+
+	lyric[6] = new Array()
+	lyric[6][0] = 'Now'
+	lyric[6][1] = "I'm"
+	lyric[6][2] = 'falling'
+	lyric[6][3] = 'asleep'
+	
+	lyric[7] = new Array()
+	lyric[7][0] = 'And'
+	lyric[7][1] = "it's"
+	lyric[7][2] = 'all'
+	lyric[7][3] = 'in'
+	lyric[7][4] = 'my'
+	lyric[7][5] = 'head'
+
+	lyric[8] = new Array()
+	lyric[8][0] = 'And'
+	lyric[8][1] = 'my'
+	lyric[8][2] = 'stomach'
+	lyric[8][3] = 'is'
+	lyric[8][4] = 'sick'
+
+	lyric[9] = new Array()
+	lyric[9][0] = 'Now'
+	lyric[9][1] = "they're"
+	lyric[9][2] = 'going'
+	lyric[9][3] = 'to'
+	lyric[9][4] = 'bed'
+
+	lyric[10] = new Array()
+	lyric[10][0] = 'And'
+	lyric[10][1] = "she's"
+	lyric[10][2] = 'taking'
+	lyric[10][3] = 'a'
+	lyric[10][4] = 'drag'
+
+	lyric[11] = new Array()
+	lyric[11][0] = 'And'
+	lyric[11][1] = "she's"
+	lyric[11][2] = 'calling'
+	lyric[11][3] = 'a'
+	lyric[11][4] = 'cab'
+	
+	lyric[12] = new Array()
+	lyric[12][0] = 'But'
+	lyric[12][1] = "she's"
+	lyric[12][2] = 'touching'
+	lyric[12][3] = 'his'
+	lyric[12][4] = 'chest'
+
+	lyric[13] = new Array()
+	lyric[13][0] = 'Now'
+	lyric[13][1] = ','
+	lyric[13][2] = 'he'
+	lyric[13][3] = 'takes'
+	lyric[13][4] = 'off'
+	lyric[13][5] = 'her'
+	lyric[13][5] = 'dress'
+
+	lyric[14] = new Array()
+	lyric[14][0] = 'Now'
+	lyric[14][1] = ','
+	lyric[14][2] = 'letting'
+	lyric[14][3] = 'me'
+	lyric[14][4] = 'go'
+
+	lyric[15] = new Array()
+	lyric[15][0] = 'I'
+	lyric[15][1] = 'just'
+	lyric[15][2] = "can't"
+	lyric[15][3] = 'look'
+	lyric[15][4] = "it's"
+	lyric[15][5] = 'killing'
+	lyric[15][6] = 'me'
+
+	lyric[16] = new Array()
+	lyric[16][0] = 'And'
+	lyric[16][1] = 'taking'
+	lyric[16][2] = 'control'
+	
+	lyric[17] = new Array()
+	lyric[17][0] = 'Jealousy'
+	lyric[17][1] = ','
+	lyric[17][2] = 'turning'
+	lyric[17][3] = 'saints'
+	lyric[17][4] = 'into'
+	lyric[17][5] = 'the'
+	lyric[17][5] = 'sea'
+
+	lyric[18] = new Array()
+	lyric[18][0] = 'Swimming'
+	lyric[18][1] = 'through'
+	lyric[18][2] = 'sick'
+	lyric[18][3] = 'lullabies'
+	   
+	lyric[19] = new Array()
+	lyric[19][0] = 'Choking'
+	lyric[19][1] = 'on'
+	lyric[19][2] = 'your'
+	lyric[19][3] = 'alibis'
+	   
+	lyric[20] = new Array()
+	lyric[20][0] = 'But'
+	lyric[20][1] = "it's"
+	lyric[20][2] = 'just'
+	lyric[20][3] = 'the'
+	lyric[20][4] = 'price'
+	lyric[20][5] = 'I'
+	lyric[20][6] = 'pay'
+
+	lyric[21] = new Array()
+	lyric[21][0] = 'Destiny'
+	lyric[21][1] = 'is'
+	lyric[21][2] = 'calling'
+	lyric[21][3] = 'me'
+	   
+	lyric[22] = new Array()
+	lyric[22][0] = 'Open'
+	lyric[22][1] = 'up'
+	lyric[22][2] = 'my'
+	lyric[22][3] = 'eager'
+	lyric[22][4] = 'eyes'
+	    
+	lyric[23] = new Array()
+	lyric[23][0] = 'Cause'
+	lyric[23][1] = "I'm"
+	lyric[23][2] = 'Mr'
+	lyric[23][3] = '.'
+	lyric[23][4] = 'Brightside'
+
+	
+function loadSong(){
+	var artist = "The Killers";
+	var song = "Mr. Brideside";
+	
+	document.getElementById('lblArtist').innerHTML = 'Artist: ' + artist;
+	document.getElementById('lblSong').innerHTML = 'Song: ' + song;
+
+	var node = document.createElement('label');
+	node.innerHTML = '</br>';
+	document.getElementById('container').appendChild(node);
+	
+	for (var i = 0; i < lyric.length; i++) {
+		var matrix = lyric[i];
+		for (var j = 0; j < matrix.length; j++) {
+			var node = document.createElement('label');
+			var word = matrix[j];
+			var word2 = Array(word.length+1).join('*')
+			node.innerHTML = '<label id="lbl' + i + j + '">' + word2 + '&nbsp</label>';
+			document.getElementById('container').appendChild(node);
+		}
+		
+		var node = document.createElement('label');
+		node.innerHTML = '</br>';
+		document.getElementById('container').appendChild(node);
+	
+	}
+}
+
+function guessWord(guess) {
+	if(guess.length > 0) {
+			var entryArray = guess.split(" ");
+			for (var x = 0; x < entryArray.length; x++) {
+				for (var i =  0; i < lyric.length; i++) {
+					var matrix = lyric[i];
+					for (var j = 0; j < matrix.length; j++) {
+							var word = matrix[j];
+							if (entryArray[x].toLowerCase() == word.toLowerCase()) {
+									document.getElementById('lbl'+i+j).innerHTML = word + '&nbsp';
+									document.getElementById('lbl'+i+j).style.color = "blue";
+									
+							}
+					}
+				}
+			}
+			document.getElementById('txtEnter').value = ""	
+	}
+}
+
+
+/*lyric = "
+Coming out of my cage
+And I\'ve been doing just fine
+Gotta gotta be down
+Because I want it all
+It started out with a kiss
+How did it end up like this
+It was only a kiss, it was only a kiss
+Now I\'m falling asleep
+And she\'s calling a cab
+While he\'s having a smoke
+And she\'s taking a drag
+Now they\'re going to bed
+And my stomach is sick
+And it\'s all in my head
+But she\'s touching his chest
+Now, he takes off her dress
+Now, letting me go
+
+I just can\'t look its killing me
+And taking control
+Jealousy, turning saints into the sea
+Swimming through sick lullabies
+Choking on your alibis
+But it\'s just the price I pay
+Destiny is calling me
+Open up my eager eyes
+Cause I\'m Mr Brightside
+
+I\'m coming out of my cage
+And I\'ve been doing just fine
+Gotta gotta be down
+Because I want it all
+It started out with a kiss
+How did it end up like this
+It was only a kiss, it was only a kiss
+Now I\'m falling asleep
+And she\'s calling a cab
+While he\'s having a smoke
+And she\'s taking a drag
+Now they\'re going to bed
+And my stomach is sick
+And it\'s all in my head
+But she\'s touching his chest
+Now, he takes off her dress
+Now, letting me go
+
+\'Cause I just can\'t look its killing me
+And taking control
+Jealousy, turning saints into the sea
+Swimming through sick lullabies
+Choking on your alibi
+But it\'s just the price I pay
+Destiny is calling me
+Open up my eager eyes
+\'Cause I\'m Mr Brightside
+I never
+I never
+I never
+I never"
+*/
